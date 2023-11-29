655303568ab515756a14b0d9a0c1f7d910494c60
Matheus Gabriel <matheusgmello1@gmail.com>
2023-11-29 15:36:10 -0300

    Feat: Fórum com DDD & Clean Architecture - Caso de uso: Listar perguntas recentes

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,7 +1,7 @@
+import { Slug } from './value-objects/slug'
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
-import { Slug } from './value-objects/slug'
 import dayjs from 'dayjs'
 
 export interface QuestionProps {
@@ -80,7 +80,7 @@ export class Question extends Entity<QuestionProps> {
       {
         ...props,
         slug: props.slug ?? Slug.createFromText(props.title),
-        createdAt: new Date(),
+        createdAt: props.createdAt ?? new Date(),
       },
       id,
     )
