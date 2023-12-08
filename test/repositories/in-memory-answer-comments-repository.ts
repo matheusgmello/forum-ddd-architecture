53a186bc24694a40534edbb2f49d58beafa92105
Matheus Gabriel <matheusgmello1@gmail.com>
2023-12-08 16:49:43 -0300

    Feat:  Subdomínios & Domain Events - Ouvindo um evento de domínio

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,39 +1,52 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
-import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
-import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
-export class InMemoryAnswerCommentsRepository
-  implements AnswerCommentsRepository
-{
-  public items: AnswerComment[] = []
+export class InMemoryAnswersRepository implements AnswersRepository {
+  public items: Answer[] = []
+
+  constructor(
+    private answerAttachmentsRepository: AnswerAttachmentsRepository,
+  ) {}
 
   async findById(id: string) {
-    const answerComment = this.items.find((item) => item.id.toString() === id)
+    const answer = this.items.find((item) => item.id.toString() === id)
 
-    if (!answerComment) {
+    if (!answer) {
       return null
     }
 
-    return answerComment
+    return answer
   }
 
-  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
-    const answerComments = this.items
-      .filter((item) => item.answerId.toString() === answerId)
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
       .slice((page - 1) * 20, page * 20)
 
-    return answerComments
+    return answers
   }
 
-  async create(answerComment: AnswerComment) {
-    this.items.push(answerComment)
+  async create(answer: Answer) {
+    this.items.push(answer)
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
-  async delete(answerComment: AnswerComment) {
-    const itemIndex = this.items.findIndex(
-      (item) => item.id === answerComment.id,
-    )
+  async save(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items[itemIndex] = answer
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
+  }
+
+  async delete(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
 
     this.items.splice(itemIndex, 1)
+    this.answerAttachmentsRepository.deleteManyByAnswerId(answer.id.toString())
   }
 }
